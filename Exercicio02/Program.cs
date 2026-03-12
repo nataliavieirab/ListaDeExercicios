918e97b2c88238b053ef2aa84d2b86575fc973fa
Natalia Vieira <nataliabortolivieira@gmail.com>
2026-03-12 16:27:02 -0300

    exercicio02

---
+++ Exercicio02/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Digite o raio do cilindro: ");
+decimal raio = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Digite a altura do cilindro: ");
+decimal altura = Convert.ToDecimal(Console.ReadLine());
+
+decimal areaBase = 3.14m * raio * raio;
+
+decimal volume = areaBase * altura;
+
+Console.WriteLine($"O volume do cilindro é: {volume.ToString("F2")} cm³");
+
+Console.ReadLine();
