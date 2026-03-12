061ab93e7b966489f1f8c3dcd116d9c817d5b4b8
Natalia Vieira <nataliabortolivieira@gmail.com>
2026-03-12 16:28:57 -0300

    exercicio03

---
+++ Exercicio03/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Digite a quilometragem inicial do veículo: ");
+decimal kmInicial = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Digite a quilometragem final do veículo: ");
+decimal kmFinal = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Digite a quantidade de combustível consumida (litros): ");
+decimal combustivelConsumido = Convert.ToDecimal(Console.ReadLine());
+
+decimal distanciaPercorrida = kmFinal - kmInicial;
+
+decimal consumoLitrosPorKm = distanciaPercorrida / combustivelConsumido;
+
+Console.WriteLine($"O consumo de combústivel durante o percurso foi de ~: {consumoLitrosPorKm} km/l");
+
+Console.ReadLine();
