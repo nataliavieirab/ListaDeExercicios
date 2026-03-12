c78fd422b187a390352289517f75bf76352db46b
Natalia Vieira <nataliabortolivieira@gmail.com>
2026-03-12 20:03:47 -0300

    feat: Adicionado Exercicio 09

---
+++ Exercicio09/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("=====================================================");
+Console.WriteLine("Calcular Área de um Retângulo!");
+Console.WriteLine("=====================================================");
+Console.WriteLine();
+
+decimal comprimento, largura, area;
+
+Console.Write("Digite o valor do comprimento: ");
+comprimento = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Digite o valor da largura: ");
+largura = Convert.ToDecimal(Console.ReadLine());
+
+area = comprimento * largura;
+
+Console.WriteLine();
+Console.WriteLine($">> A área do terreno é: {area} m²");
+
+Console.ReadLine();
