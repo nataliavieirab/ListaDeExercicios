92bae398569163dda58bd1f85c99f86241f73811
Natalia Vieira <nataliabortolivieira@gmail.com>
2026-03-12 17:06:11 -0300

    feat: Adicionado Exercício 05

---
+++ Exercicio05/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Digite o salário base do vendedor: ");
+decimal salarioBase = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Digite o total de vendas: ");
+decimal totalVendas = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Digite o percentual: ");
+decimal percentual = Convert.ToDecimal(Console.ReadLine()) / 100;
+
+decimal comissao = percentual * totalVendas;
+decimal salarioTotal = salarioBase + comissao;
+
+Console.WriteLine("-----------------------------------------------------");
+
+Console.WriteLine($"Comissão: {comissao}");
+Console.WriteLine($"Salário total do vendedor: {salarioTotal}");
+
+Console.ReadLine();
