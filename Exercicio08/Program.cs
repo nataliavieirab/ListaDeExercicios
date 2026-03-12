bc8033ef0f2b723867be2f79524eda0ca7589330
Natalia Vieira <nataliabortolivieira@gmail.com>
2026-03-12 19:40:47 -0300

    feat: Adicionado Exercicio 08

---
+++ Exercicio08/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("=====================================================");
+Console.WriteLine("Verificar Número Primo!");
+Console.WriteLine("=====================================================");
+Console.WriteLine();
+
+Console.Write("Digite um número: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+if (numero <= 1)
+{
+  Console.Write($">> {numero} não é um número primo!");
+  return;
+}
+
+for (int i = 2; i < numero; i++)
+{
+  if (numero % i == 0)
+  {
+    Console.Write($">> {numero} não é um número primo!");
+    return;
+  }
+}
+
+Console.Write($">> {numero} é um número primo!");
+
+Console.ReadLine();
