325104df5a0faa425954dba85583c7ad73a4c42b
Natalia Vieira <nataliabortolivieira@gmail.com>
2026-03-12 16:23:56 -0300

    exercicio01

---
+++ Exercicio01/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Digite o comprimento da caixa: ");
+decimal comprimento = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Digite a largura da caixa: ");
+decimal largura = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Digite a altura da caixa: ");
+decimal altura = Convert.ToDecimal(Console.ReadLine());
+
+decimal volume = comprimento * largura * altura;
+
+Console.WriteLine($"O volume da caixa retangular é: {volume} cm³");
+
+Console.ReadLine();
