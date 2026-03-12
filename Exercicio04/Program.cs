720a97b52b64b29677be30377118e17a919aafb8
Natalia Vieira <nataliabortolivieira@gmail.com>
2026-03-12 16:34:21 -0300

    exercicio04

---
+++ Exercicio04/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Digite a temperatura em Celsius: ");
+decimal celsius = Convert.ToDecimal(Console.ReadLine());
+
+decimal fahrenheit = (9m / 5m * celsius) + 32;
+
+Console.WriteLine($"{celsius}°C = {fahrenheit}°F");
+
+Console.ReadLine();
