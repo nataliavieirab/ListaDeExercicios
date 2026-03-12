d3e6261ccf723d226162874751dd2a7d445ee017
Natalia Vieira <nataliabortolivieira@gmail.com>
2026-03-12 18:22:00 -0300

    feat: Adicionado Exercício 06

---
+++ Exercicio06/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("-----------------------------------------------------");
+Console.WriteLine("Calculo Média Harmônica!");
+Console.WriteLine("-----------------------------------------------------");
+
+Console.Write("Digite a quantidade de notas a serem calculadas: ");
+int quantidadeNotas = Convert.ToInt32(Console.ReadLine());
+decimal soma = 0;
+
+Console.WriteLine("-----------------------------------------------------");
+for (int i = 1; i <= quantidadeNotas; i++)
+{
+  decimal nota;
+
+  while (true)
+  {
+    Console.Write($"{i}- Digite a nota: ");
+    nota = Convert.ToDecimal(Console.ReadLine());
+
+    if (nota != 0)
+      break;
+
+    Console.WriteLine("Nota não pode ser zero.");
+  }
+
+  decimal inversoNota = 1 / nota;
+  soma += inversoNota;
+}
+
+decimal mediaHarmonica = quantidadeNotas / soma;
+
+Console.WriteLine("-----------------------------------------------------");
+Console.WriteLine($"Média Harmônica: {mediaHarmonica:F2}");
+
+Console.ReadLine();
