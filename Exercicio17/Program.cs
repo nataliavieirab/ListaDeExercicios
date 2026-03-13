2f4706e2e49c6a71b3c177265f19fa3ea055abd8
Natalia Vieira <nataliabortolivieira@gmail.com>
2026-03-13 17:24:34 -0300

    feat: Adicionado Exercicio 17 e 18

---
+++ Exercicio17/Program.cs
@@ -0,0 +1,23 @@
+﻿int a, b, c;
+string operacao;
+
+Console.Write("Digite o primeiro valor: ");
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o segundo valor: ");
+b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b)
+{
+  c = a + b;
+  operacao = "soma";
+}
+else
+{
+  c = a * b;
+  operacao = "multiplicação";
+}
+
+Console.WriteLine($"O resultado da {operacao} dos valores é {c}");
+
+Console.ReadLine();
---
+++ Exercicio18/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("=====================================================");
+Console.WriteLine("Números Ímpares entre 100 e 200");
+Console.WriteLine("=====================================================");
+Console.WriteLine();
+
+List<int> numerosImpares = [];
+
+for (int i = 100; i <= 200; i++)
+{
+  if (i % 2 != 0)
+  {
+    numerosImpares.Add(i);
+  }
+  continue;
+}
+
+Console.WriteLine(string.Join(", ", numerosImpares));
