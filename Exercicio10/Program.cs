fd48e639b0c3096c372603cf9aabc2a338693b05
Natalia Vieira <nataliabortolivieira@gmail.com>
2026-03-13 14:59:54 -0300

    feat: Adicionado Exercicio 10

---
+++ Exercicio10/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("=====================================================");
+Console.WriteLine("Calcular Valor Arrecadado");
+Console.WriteLine("=====================================================");
+Console.WriteLine();
+
+Console.Write("Digite a quantidade de pães vendidos: ");
+int qtdPaes = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite a quantidade de broas vendidas: ");
+int qtdBroas = Convert.ToInt32(Console.ReadLine());
+
+decimal valorArrecadado = (qtdPaes * 0.12m) + qtdBroas * 1.50m;
+
+decimal valorPoupanca = valorArrecadado * 0.10m;
+
+Console.WriteLine();
+Console.WriteLine($"O valor total arrecadado foi: R{valorArrecadado:C}");
+Console.WriteLine($"Valor para poupança: R{valorPoupanca:C}");
