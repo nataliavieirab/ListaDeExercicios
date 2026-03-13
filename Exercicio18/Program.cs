74f45cbc291e26096c126d7a413f7c6503d9b7df
Natalia Vieira <nataliabortolivieira@gmail.com>
2026-03-13 18:44:52 -0300

    refactor: Exercicio 18

--- Exercicio18/Program.cs
+++ Exercicio18/Program.cs
@@ -1,17 +1,13 @@
 ﻿Console.WriteLine("=====================================================");
-Console.WriteLine("Números Ímpares entre 100 e 200");
+Console.WriteLine("NÚMEROS ÍMPARES ENTRE 100 E 200");
 Console.WriteLine("=====================================================");
 Console.WriteLine();
-
-List<int> numerosImpares = [];
 
 for (int i = 100; i <= 200; i++)
 {
   if (i % 2 != 0)
-  {
-    numerosImpares.Add(i);
-  }
-  continue;
+    Console.Write($"{i} - ");
 }
 
-Console.WriteLine(string.Join(", ", numerosImpares));
+Console.WriteLine();
+Console.ReadLine();
