fc9e6190230b372548b1af409c751deb52037b34
Natalia Vieira <nataliabortolivieira@gmail.com>
2026-03-13 18:44:19 -0300

    feat: Adicionado Exercicio 19

---
+++ Exercicio19/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("=====================================================");
+Console.WriteLine("SOMA DE NUMEROS ÍMPARES MÚLTIPLOS DE 3 ENTRE 1 E 500");
+Console.WriteLine("=====================================================");
+Console.WriteLine();
+
+int soma = 0;
+
+for (int i = 1; i <= 500; i++)
+{
+  if (i % 2 != 0 && i % 3 == 0)
+    soma += i;
+}
+
+Console.WriteLine($"Resultado:");
+Console.WriteLine($">> {soma}");
+
+Console.ReadLine();
