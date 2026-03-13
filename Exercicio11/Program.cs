d402a40761f6c6de6de0839b0c3080ef721d304e
Natalia Vieira <nataliabortolivieira@gmail.com>
2026-03-13 17:39:11 -0300

    feat: Adicionado Exercício 11 e 12

---
+++ Exercicio11/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("=====================================================");
+Console.WriteLine("Quantos dias você já viveu?");
+Console.WriteLine("=====================================================");
+Console.WriteLine();
+
+Console.Write("Digite seu nome: ");
+string? nome = Console.ReadLine();
+
+Console.Write("Digite sua idade: ");
+int idade = Convert.ToInt32(Console.ReadLine());
+
+int diasDeVida = idade * 365;
+
+Console.WriteLine();
+Console.WriteLine($"{nome!.ToUpper()}, VOCÊ JÁ VIVEU {diasDeVida} DIAS! :)");
+
+Console.ReadLine();
---
+++ Exercicio12/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("=====================================================");
+Console.WriteLine("CÁLCULO DE SALÁRIO (AUMENTO E DESCONTO DE IMPOSTO)");
+Console.WriteLine("=====================================================");
+Console.WriteLine();
+
+Console.Write("Digite o salário do funcionário: ");
+decimal salarioInicial = Convert.ToDecimal(Console.ReadLine());
+
+decimal salarioComAumento = salarioInicial + (salarioInicial * 0.15m);
+decimal valorImposto = salarioComAumento * 0.08m;
+decimal salarioFinal = salarioComAumento - valorImposto;
+
+Console.WriteLine();
+Console.WriteLine("--------------- RESULTADO ----------------");
+Console.WriteLine($"Salário inicial: R$ {salarioInicial}");
+Console.WriteLine($"Salário com aumento de 15%: R$ {salarioComAumento}");
+Console.WriteLine($"Salário final após desconto de 8%: R$ {salarioFinal:F2}");
+Console.WriteLine("------------------------------------------");
