c4af1b6293076b01c5d6642fb6faac013bdcbadb
Natalia Vieira <nataliabortolivieira@gmail.com>
2026-03-13 19:09:05 -0300

    feat: Exercicio 20

---
+++ Exercicio20/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine();
+Console.Write("Digite o número da tabuada que você quer gerar: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+Console.WriteLine($"=============== TABUADA do {numero} ===============");
+Console.WriteLine();
+
+for (int i = 0; i <= 10; i++)
+{
+  int resultado = i * numero;
+  Console.WriteLine($"{numero} x {i} = {resultado}");
+  Console.WriteLine();
+}
+
+Console.ReadLine();
