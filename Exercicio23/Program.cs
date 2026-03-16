e50f543de308fdd85698ab1e0fc8478dceb79e3d
Natalia Vieira <nataliabortolivieira@gmail.com>
2026-03-16 12:48:45 -0300

    feat: Exercicio 23

---
+++ Exercicio23/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("=====================================================");
+Console.WriteLine("FIZZ, BUZZ e FIZZBUZZ");
+Console.WriteLine("=====================================================");
+Console.WriteLine();
+
+for (int i = 1; i <= 100; i++)
+{
+  string resultado = "";
+
+  if (i % 3 == 0) resultado += "Fizz";
+  if (i % 5 == 0) resultado += "Buzz";
+
+  Console.Write($"{(resultado == "" ? i : resultado)}, ");
+}
