c79961f93dc4de1873a1fe0ba503507fe8e76898
Natalia Vieira <nataliabortolivieira@gmail.com>
2026-03-16 11:45:06 -0300

    feat: Exercicio 21

---
+++ Exercicio21/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("=====================================================");
+Console.WriteLine("CÁLCULO FATORIAL");
+Console.WriteLine("=====================================================");
+Console.WriteLine();
+
+Console.Write("Digite um número: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+int resultado = numero;
+
+for (int i = numero - 1; i <= 1; i--)
+{
+  resultado = resultado * i;
+}
+
+Console.WriteLine(resultado);
